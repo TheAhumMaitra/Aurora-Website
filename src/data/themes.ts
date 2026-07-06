f6bdda27b31ea9a411c292c8f2cdd11526bad48d
Ahum Maitra <theahummaitra@gmail.com>
2026-07-06 15:30:29 +0530

    fix: Change to main branch

--- src/data/themes.ts
+++ src/data/themes.ts
@@ -12,74 +12,74 @@ export const themes: Theme[] = [
   {
     id: "0001",
     name: "Aurora Default",
-    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/hyprland-git-lua/dotfiles/.config/themes/Aurora%20Default",
+    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/master/dotfiles/.config/themes/Aurora%20Default",
     status: "official",
     previewImageUrl:
-      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/hyprland-git-lua/dotfiles/.config/themes/Aurora%20Default/preview.png",
+      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/master/dotfiles/.config/themes/Aurora%20Default/preview.png",
   },
   {
     id: "0002",
     name: "Aurora Glassy",
-    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/hyprland-git-lua/dotfiles/.config/themes/Aurora%20glassy",
+    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/master/dotfiles/.config/themes/Aurora%20glassy",
     status: "official",
     previewImageUrl:
-      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/hyprland-git-lua/dotfiles/.config/themes/Aurora%20glassy/preview.png",
+      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/master/dotfiles/.config/themes/Aurora%20glassy/preview.png",
   },
   {
     id: "0003",
     name: "Catppuccin Mocha",
-    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/hyprland-git-lua/dotfiles/.config/themes/Catppuccin%20Mocha",
+    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/master/dotfiles/.config/themes/Catppuccin%20Mocha",
     status: "official",
     previewImageUrl:
-      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/hyprland-git-lua/dotfiles/.config/themes/Catppuccin%20Mocha/preview.png",
+      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/master/dotfiles/.config/themes/Catppuccin%20Mocha/preview.png",
   },
   {
     id: "0004",
     name: "Dracula",
-    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/hyprland-git-lua/dotfiles/.config/themes/Dracula",
+    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/master/dotfiles/.config/themes/Dracula",
     status: "official",
     previewImageUrl:
-      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/hyprland-git-lua/dotfiles/.config/themes/Dracula/preview.png",
+      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/master/dotfiles/.config/themes/Dracula/preview.png",
   },
   {
     id: "0005",
     name: "Everforest",
-    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/hyprland-git-lua/dotfiles/.config/themes/Everforest",
+    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/master/dotfiles/.config/themes/Everforest",
     status: "official",
     previewImageUrl:
-      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/hyprland-git-lua/dotfiles/.config/themes/Everforest/preview.png",
+      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/master/dotfiles/.config/themes/Everforest/preview.png",
   },
   {
     id: "0006",
     name: "Gruvbox",
-    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/hyprland-git-lua/dotfiles/.config/themes/Gruvbox",
+    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/master/dotfiles/.config/themes/Gruvbox",
     status: "official",
     previewImageUrl:
-      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/hyprland-git-lua/dotfiles/.config/themes/Gruvbox/preview.png",
+      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/master/dotfiles/.config/themes/Gruvbox/preview.png",
   },
   {
     id: "0007",
     name: "Nord",
-    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/hyprland-git-lua/dotfiles/.config/themes/Nord",
+    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/master/dotfiles/.config/themes/Nord",
     status: "official",
     previewImageUrl:
-      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/hyprland-git-lua/dotfiles/.config/themes/Nord/preview.png",
+      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/master/dotfiles/.config/themes/Nord/preview.png",
   },
   {
     id: "0008",
     name: "Rose Pine",
-    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/hyprland-git-lua/dotfiles/.config/themes/Rose%20Pine",
+    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/master/dotfiles/.config/themes/Rose%20Pine",
     status: "official",
     previewImageUrl:
-      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/hyprland-git-lua/dotfiles/.config/themes/Rose%20Pine/preview.png",
+      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/master/dotfiles/.config/themes/Rose%20Pine/preview.png",
   },
   {
     id: "0009",
     name: "Tokyo Night",
-    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/hyprland-git-lua/dotfiles/.config/themes/Tokyo%20Night",
+    repoUrl: "https://github.com/TheAhumMaitra/Aurora/tree/master/dotfiles/.config/themes/Tokyo%20Night",
     status: "official",
     previewImageUrl:
-      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/hyprland-git-lua/dotfiles/.config/themes/Tokyo%20Night/preview.png",
+      "https://raw.githubusercontent.com/TheAhumMaitra/Aurora/refs/heads/master/dotfiles/.config/themes/Tokyo%20Night/preview.png",
   },
   {
     id: "0010",
